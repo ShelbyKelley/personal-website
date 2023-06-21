afc8e873ba24a58a139420d8c68f2f0a7f879f34
Shelby Kelley <shelbya.kelley@gmail.com>
2023-06-20 23:15:42 -0600

    add home and page not found, plus styled navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { AboutComponent } from "./about/about.component";
 import { BlogComponent } from "./blog/blog.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavBarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
@@ -18,8 +23,17 @@ import { FooterComponent } from './footer/footer.component';
     ProjectsComponent,
     NavBarComponent,
     FooterComponent,
+    HomeComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FontAwesomeModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
