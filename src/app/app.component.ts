d4e2fc6fc2750ddc0f04a3d702b591ab71e4404f
Shelby Kelley <shelbya.kelley@gmail.com>
2023-07-21 23:32:59 -0600

    responsive setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'personal-website';
+
+  constructor(private readonly _responsive: BreakpointObserver) {}
+
+  ngOnInit() {
+    this._responsive.observe([Breakpoints.Handset, Breakpoints.Tablet]).subscribe(result => {
+      if (result.matches) {
+        console.log('Change Header and Footer');
+      } else {
+        console.log('Desktop');
+      }
+    });
+  }
 }
